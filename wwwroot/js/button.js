6024f30d0a2803ae3ac36730da6f1879475bc34a
Ata Doruk Çakmak <atadoruk68@gmail.com>
2024-03-28 17:12:15 +0300

    Second button created. Sum of the numbers are written on screen

--- wwwroot/js/button.js
+++ wwwroot/js/button.js
@@ -1,8 +1,32 @@
-function myFunction() {
-    var x = document.getElementById("myDIV");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+function HideShowFunction(){
+  var x=document.getElementById("myDIV")
+  if(x.style.display==="none"){
+      x.style.display="block";
+      console.log("block");
+  }else {
+      x.style.display="none";
+      console.log("none");
   }
+}
+
+function HideShow2Function(){
+  var x=document.getElementById("result")
+  if(x.style.display==="none"){
+      x.style.display="none";
+      console.log("none");
+  }else {
+      x.style.display="block";
+      console.log("block");
+  }
+  
+}
+
+function toggleBox() {
+  document.getElementById("result").style.display = "block";
+  
+  const num1 = document.getElementById('num1').value;
+  const num2 = document.getElementById('num2').value;
+  const sum = parseInt(num1) + parseInt(num2);
+  document.getElementById('result').innerText = `Toplam: ${sum}`;
+
+}
